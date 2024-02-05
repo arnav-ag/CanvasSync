106cb37435acea016134829c99fc5554e1976b27
arnav-ag <arnav.ctf@gmail.com>
2024-02-05 12:16:35 +0800

    Debug logging for debugging :)

--- canvas.py
+++ canvas.py
@@ -33,7 +33,7 @@ logger.propagate = False
 
 def setup_cron_job(script_path, add_job):
     cron = CronTab(user=True)
-    job_command = f'{sys.executable} {script_path} run'
+    job_command = f'{sys.executable} {script_path} --debug run > {os.path.join(os.path.dirname(__file__), "canvas.log")} 2>&1'
 
     # Find existing job
     existing_job = None
